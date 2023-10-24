608e780251615ce6e6969799d677766152814a7d
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-10-24 22:47:29 +0200

    adding createTicket function

--- server/dao.js
+++ server/dao.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const sqlite = require('sqlite3');
+const sqlite = require("sqlite3");
 
 // open the database
-const db = new sqlite.Database('se2-01-mock.sqlite', (err) => {
-  if(err) throw err;
+const db = new sqlite.Database("se2-01-mock.sqlite", (err) => {
+  if (err) throw err;
 });
 async function getNextCustomer(counterId) {
   return new Promise(async (resolve, reject) => {
@@ -24,62 +24,71 @@ async function getNextCustomer(counterId) {
       const queueLengths = await getQueueLengths(supportedServiceTypes);
 
       if (queueLengths.length === 0) {
-        return null; 
+        return null;
       }
 
       let longestQueue = queueLengths[0];
 
       for (let i = 1; i < queueLengths.length; i++) {
-        if (queueLengths[i].queueLength > longestQueue.queueLength ||
-            (queueLengths[i].queueLength === longestQueue.queueLength && 
-             queueLengths[i].waitingTime < longestQueue.waitingTime)) {
+        if (
+          queueLengths[i].queueLength > longestQueue.queueLength ||
+          (queueLengths[i].queueLength === longestQueue.queueLength &&
+            queueLengths[i].waitingTime < longestQueue.waitingTime)
+        ) {
           longestQueue = queueLengths[i];
         }
       }
 
-      db.get(`
+      db.get(
+        `
       SELECT t.ticketID as TicketID
       FROM tickets t
       WHERE t.serviceID = ?    
       AND t.Status = 'waiting'
       ORDER BY t.ticketID ASC
       LIMIT 1
-      `, [longestQueue.serviceType], (err, row) => {
-      if (err) {
-        reject(new Error('Failed to get the next customer.'));
-      } else {
-        if (row) {
-          const customer = {
-            customerID: row.CustomerID,
-            ticketID: row.TicketID
-          };
-          resolve(customer);
-        } else {
-          resolve(null);
+      `,
+        [longestQueue.serviceType],
+        (err, row) => {
+          if (err) {
+            reject(new Error("Failed to get the next customer."));
+          } else {
+            if (row) {
+              const customer = {
+                customerID: row.CustomerID,
+                ticketID: row.TicketID,
+              };
+              resolve(customer);
+            } else {
+              resolve(null);
+            }
+          }
         }
-      }
-      });
+      );
     } catch (error) {
       reject(error);
     }
   });
 }
 
-
 async function getSupportedServiceTypes(counterId) {
   return new Promise((resolve, reject) => {
-    db.all(`
+    db.all(
+      `
       SELECT *
       FROM counters c
       WHERE c.counterID = ?
-    `, [counterId], (err, rows) => {
-      if (err) {
-        reject(new Error('Failed to get supported service types.'));
-      } else {
-        const supportedServiceTypes = rows.map(row => row.serviceID);
-        resolve(supportedServiceTypes);
+    `,
+      [counterId],
+      (err, rows) => {
+        if (err) {
+          reject(new Error("Failed to get supported service types."));
+        } else {
+          const supportedServiceTypes = rows.map((row) => row.serviceID);
+          resolve(supportedServiceTypes);
+        }
       }
-    });
+    );
   });
 }
 
@@ -88,28 +97,49 @@ async function getQueueLengths(supportedServiceTypes) {
     const queueLengths = [];
 
     for (const serviceType of supportedServiceTypes) {
-      db.get(`
+      db.get(
+        `
         SELECT COUNT(t.ticketID) as QueueLength, s.waitingTime as ServiceTime
         FROM tickets t
         INNER JOIN services s ON t.serviceID = s.serviceID
         WHERE t.serviceID = ? AND t.status = 'waiting'
-      `, [serviceType], (err, row) => {
-        if (err) {
-          reject(new Error('Failed to get queue lengths.'));
-        } else {
-          queueLengths.push({
-            serviceType: serviceType,
-            queueLength: row.QueueLength || 0,
-            waitingTime: row.ServiceTime 
-          });
+      `,
+        [serviceType],
+        (err, row) => {
+          if (err) {
+            reject(new Error("Failed to get queue lengths."));
+          } else {
+            queueLengths.push({
+              serviceType: serviceType,
+              queueLength: row.QueueLength || 0,
+              waitingTime: row.ServiceTime,
+            });
 
-          if (queueLengths.length === supportedServiceTypes.length) {
-            resolve(queueLengths);
+            if (queueLengths.length === supportedServiceTypes.length) {
+              resolve(queueLengths);
+            }
           }
         }
-      });
+      );
     }
   });
 }
 
-
+async function createTicket(serviceId) {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `
+      INSERT INTO tickets (serviceID, status)
+      VALUES (?, '0')
+    `,
+      [serviceId],
+      function (err) {
+        if (err) {
+          reject(new Error("Failed to create ticket."));
+        } else {
+          resolve({ ticketId: this.lastID, customerId });
+        }
+      }
+    );
+  });
+}
