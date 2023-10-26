fbd06558d2ae0e9bccc31d5fdaec248ed601d233
mcisthn504 <S319903@studenti.polito.it>
2023-10-26 16:35:01 +0200

    first setup(a mock)

---
+++ client/tests/unit/QueueManagement.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import QueueManagement from '../../src/QueManagement.jsx';
+
+// Mock the props you want to pass to the component
+const mockProps = {
+  loggedIn: false, // You can adjust these props based on your test scenarios
+  doLogout: jest.fn(),
+};
+
+test('QueueManagement renders correctly', () => {
+  const { getByText } = render(<QueueManagement {...mockProps} />);
+  
+  // You can use queries like getByText, getByTestId, etc. to check if specific elements are rendered correctly.
+  expect(getByText('Queue Management System')).toBeInTheDocument();
+});
+
+test('Get Ticket button works as expected', () => {
+  const { getByText } = render(<QueueManagement {...mockProps} />);
+  const getTicketButton = getByText('Get Ticket');
+
+  fireEvent.click(getTicketButton);
+
+  // You can assert the expected behavior based on the component's state changes or UI updates.
+  // For example, check if the waiting time is displayed after clicking the button.
+  expect(getByText('Your Estimated Waiting Time is :')).toBeInTheDocument();
+});
+
+// Add more test cases to cover different components, states, and behaviors.
