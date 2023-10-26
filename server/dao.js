743aa203274a983ee187a1a2f82f06e35f3fd8cf
Federico Buccellato <fedebucce@gmail.com>
2023-10-26 14:20:43 +0100

    merge

--- server/dao.js
+++ server/dao.js
@@ -3,110 +3,105 @@
 const sqlite = require("sqlite3");
 const crypto = require('crypto');
 
-// open the database
+// Apri il database
 const db = new sqlite.Database("se2-01-mock.sqlite", (err) => {
   if (err) throw err;
 });
 
+/// Funzioni di login
 
-
-
-const databaseFunctions = {
-
-  ///* EOF Login Functions */
-
-  async getUser(username, password) {
-    return new Promise((resolve, reject) => {
-      const sql = 'SELECT * FROM users WHERE username = ?';
-      db.get(sql, [username], (err, row) => {
-        if (err) { reject(err); }
-        else if (row === undefined) { resolve(false); }
-        else {
-          const user = { userID: row.userID, username: row.username, role: row.role };
-          const salt = row.salt;
-          crypto.scrypt(password, salt, 32, (err, hashedPassword) => {
-            if (err) reject(err);
-            const passwordHex = Buffer.from(row.hash, 'hex');
-            if (!crypto.timingSafeEqual(passwordHex, hashedPassword))
-              resolve(false);
-            else resolve(user);
-          });
-        }
-      });
+async function getUser(username, password) {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM users WHERE username = ?';
+    db.get(sql, [username], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { userID: row.userID, username: row.username, role: row.role };
+        const salt = row.salt;
+        crypto.scrypt(password, salt, 32, (err, hashedPassword) => {
+          if (err) {
+            reject(err);
+          }
+          const passwordHex = Buffer.from(row.hash, 'hex');
+          if (!crypto.timingSafeEqual(passwordHex, hashedPassword)) {
+            resolve(false);
+          } else {
+            resolve(user);
+          }
+        });
+      }
     });
-  },
+  });
+}
 
-  async getUserById(id) {
-    return new Promise((resolve, reject) => {
-      const sql = 'SELECT * FROM users WHERE userID = ?';
-      db.get(sql, [id], (err, row) => {
-        if (err)
-          reject(err);
-        else if (row === undefined)
-          resolve({ error: 'Utente non trovato.' });
-        else {
-          const user = { userID: row.userID, username: row.username, role: row.role }
-          resolve(user);
-        }
-      });
+async function getUserById(id) {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM users WHERE userID = ?';
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve({ error: 'Utente non trovato.' });
+      } else {
+        const user = { userID: row.userID, username: row.username, role: row.role }
+        resolve(user);
+      }
     });
-  },
+  });
+}
 
-  ///* Start of OQM functions */
+/// Funzioni OQM
 
-  async getNextCustomer(counterId) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const supportedServiceTypes = await this.getSupportedServiceTypes(
-          counterId
-        );
+async function getNextCustomer(counterId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const supportedServiceTypes = await getSupportedServiceTypes(counterId);
 
-        if (supportedServiceTypes.length === 0) {
-          resolve(null);
-          return;
-        }
+      if (supportedServiceTypes.length === 0) {
+        resolve(null);
+        return;
+      }
 
-        // Qua si deve ancora creare l'azzeramento della lista ogni giorno
-        // if (UnNuovoGiorno()) {
-        //   await resetQueues();
-        // }
+      const queueLengths = await getQueueLengths(supportedServiceTypes);
 
-        const queueLengths = await this.getQueueLengths(supportedServiceTypes);
+      if (queueLengths.length === 0) {
+        resolve(null);
+        return;
+      }
 
-        if (queueLengths.length === 0) {
-          return null;
-        }
-
-        let longestQueue = queueLengths[0];
+      let longestQueue = queueLengths[0];
 
       for (let i = 1; i < queueLengths.length; i++) {
         if (queueLengths[i].queueLength > longestQueue.queueLength ||
-            (queueLengths[i].queueLength === longestQueue.queueLength && 
-             queueLengths[i].waitingTime < longestQueue.waitingTime)) {
+          (queueLengths[i].queueLength === longestQueue.queueLength &&
+            queueLengths[i].waitingTime < longestQueue.waitingTime)) {
           longestQueue = queueLengths[i];
         }
       }
-        
+
       db.get(`
-      SELECT t.ticketID as TicketID
-      FROM tickets t
-      WHERE t.serviceID = ?    
-      AND t.Status = 'waiting'
-      ORDER BY t.ticketID ASC
-      LIMIT 1
+        SELECT t.ticketID as TicketID
+        FROM tickets t
+        WHERE t.serviceID = ?
+        AND t.Status = 'waiting'
+        ORDER BY t.ticketID ASC
+        LIMIT 1
       `, [longestQueue.serviceType], (err, row) => {
-      if (err) {
-        reject(new Error('Failed to get the next customer.'));
-      } else {
-        if (row) {
-          const customer = {
-            ticketID: row.TicketID
-          };
-          resolve(customer);
+        if (err) {
+          reject(new Error('Failed to get the next customer.'));
         } else {
-          resolve(null);
+          if (row) {
+            const customer = {
+              ticketID: row.TicketID
+            };
+            resolve(customer);
+          } else {
+            resolve(null);
+          }
         }
-      }
       });
     } catch (error) {
       reject(error);
@@ -114,63 +109,62 @@ const databaseFunctions = {
   });
 }
 
-  async getSupportedServiceTypes(counterId) {
-    return new Promise((resolve, reject) => {
-      db.all(
-        `
-        SELECT *
-        FROM counters c
-        WHERE c.counterID = ?
-      `,
-        [counterId],
-        (err, rows) => {
-          if (err) {
-            reject(new Error("Failed to get supported service types."));
-          } else {
-            const supportedServiceTypes = rows.map((row) => row.serviceID);
-            resolve(supportedServiceTypes);
-          }
-        }
-      );
-    });
-  },
-
-  async getQueueLengths(supportedServiceTypes) {
-    return new Promise((resolve, reject) => {
-      const queueLengths = [];
-
-      for (const serviceType of supportedServiceTypes) {
-        db.get(
-          `
-          SELECT COUNT(t.ticketID) as QueueLength, s.waitingTime as ServiceTime
-          FROM tickets t
-          INNER JOIN services s ON t.serviceID = s.serviceID
-          WHERE t.serviceID = ? AND t.status = 'waiting'
-        `,
-          [serviceType],
-          (err, row) => {
-            if (err) {
-              reject(new Error("Failed to get queue lengths."));
-            } else {
-              queueLengths.push({
-                serviceType: serviceType,
-                queueLength: row.QueueLength || 0,
-                waitingTime: row.ServiceTime,
-              });
-
-          if (queueLengths.length === supportedServiceTypes.length) {
-            resolve(queueLengths);
-          }
+async function getSupportedServiceTypes(counterId) {
+  return new Promise((resolve, reject) => {
+    db.all(
+      `
+      SELECT *
+      FROM counters c
+      WHERE c.counterID = ?
+    `,
+      [counterId],
+      (err, rows) => {
+        if (err) {
+          reject(new Error("Failed to get supported service types."));
+        } else {
+          const supportedServiceTypes = rows.map((row) => row.serviceID);
+          resolve(supportedServiceTypes);
         }
       });
+  });
+}
+
+async function getQueueLengths(supportedServiceTypes) {
+  return new Promise((resolve, reject) => {
+    const queueLengths = [];
+
+    for (const serviceType of supportedServiceTypes) {
+      db.get(
+        `
+        SELECT COUNT(t.ticketID) as QueueLength, s.waitingTime as ServiceTime
+        FROM tickets t
+        INNER JOIN services s ON t.serviceID = s.serviceID
+        WHERE t.serviceID = ? AND t.status = 'waiting'
+      `,
+        [serviceType],
+        (err, row) => {
+          if (err) {
+            reject(new Error("Failed to get queue lengths."));
+          } else {
+            queueLengths.push({
+              serviceType: serviceType,
+              queueLength: row.QueueLength || 0,
+              waitingTime: row.ServiceTime,
+            });
+
+            if (queueLengths.length === supportedServiceTypes.length) {
+              resolve(queueLengths);
+            }
+          }
+        });
     }
   });
 }
 
 async function resetQueues() {
   try {
-    // Da valutare se eliminare tutti gli elementi o cambiare semplicemente lo status in modo da non eliminare i dati
-    // Devo ancora creare un timer che scade a mezzanotte che fa partire resetQueues
+    // Valuta se eliminare tutti gli elementi o cambiare lo stato in modo da non eliminare i dati
+    // Devi ancora creare un timer che scade a mezzanotte che fa partire resetQueues
     db.run(`
       DELETE FROM tickets
     `);
@@ -181,6 +175,4 @@ async function resetQueues() {
   }
 }
 
-exports.getNextCustomer = getNextCustomer;
-
-
+module.exports = { getNextCustomer, getUser, getUserById, resetQueues };
